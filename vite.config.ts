66b91901dac1c18a2755c567e6de081c1bcfc0ab
Radule23 <64592651+Radule23@users.noreply.github.com>
2025-10-28 20:56:03 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/promo-page-me/' : '/',
+//  base: mode === 'production' ? '/promo-page-me/' : '/',
+   base: "/promo-page-me/",
   server: {
     host: "::",
     port: 8080,
