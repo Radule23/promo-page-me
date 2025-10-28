37eae84787bccee6bf93deffcd3a3cd34efb8b0a
Radule23 <64592651+Radule23@users.noreply.github.com>
2025-10-28 22:55:18 +0100

    Debug: add layout skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@
 import React from "react";
 
 export default function App() {
-  return <div>App works</div>;
+   return (
+    <div id="app-root">
+      <header>Header</header>
+      <main>Content placeholder</main>
+      <footer>Footer</footer>
+    </div>
+  );
+
+
 }
 
 
