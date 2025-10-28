85c443fb1cd017d5291c8c8f2eef232149e80163
Radule23 <64592651+Radule23@users.noreply.github.com>
2025-10-28 21:02:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-//  base: mode === 'production' ? '/promo-page-me/' : '/',
-   base: "/promo-page-me/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default defineConfig({
+  base: "/promo-page-me/",   // obavezno za GitHub Pages
+  plugins: [react()],
+})
+
+
 }));
